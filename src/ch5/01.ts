1dcd99e55fed0f7d918278f5e52cee50221799ee
JSurapong <surapong.kawkangploo@gmail.com>
2023-10-06 10:34:20 +0700

    ch05

--- src/ch5/01.ts
+++ src/ch5/01.ts
@@ -1,1 +1,22 @@
 export {};
+class Computer {
+  static uniqueInstance: Computer;
+
+  private constructor() {} // block new Computer()
+
+  static getInstance() {
+    if (!Computer.uniqueInstance) {
+      Computer.uniqueInstance = new Computer();
+    }
+
+    return Computer.uniqueInstance;
+  }
+}
+
+function main() {
+  let computer1 = Computer.getInstance();
+  let computer2 = Computer.getInstance();
+  console.log(computer1 === computer2);
+}
+
+main();
--- src/ch5/02.ts
+++ src/ch5/02.ts
@@ -1,1 +1,44 @@
 export {};
+
+class ChocolateBoiler {
+  #empty: boolean = true;
+  #boiled: boolean = false;
+
+  private constructor() {}
+
+  static instance: ChocolateBoiler;
+
+  static getInstance(): ChocolateBoiler {
+    if (!ChocolateBoiler.instance) {
+      ChocolateBoiler.instance = new ChocolateBoiler();
+    }
+    return ChocolateBoiler.instance;
+  }
+
+  fill(): void {
+    if (this.isEmpty()) {
+      this.#empty = false;
+      this.#boiled = false;
+    }
+  }
+
+  drain(): void {
+    if (!this.isEmpty() && this.isBoiled()) {
+      this.#empty = true;
+    }
+  }
+
+  boil(): void {
+    if (!this.isEmpty() && !this.isBoiled()) {
+      this.#boiled = true;
+    }
+  }
+
+  isEmpty(): boolean {
+    return this.#empty;
+  }
+
+  isBoiled(): boolean {
+    return this.#boiled;
+  }
+}
